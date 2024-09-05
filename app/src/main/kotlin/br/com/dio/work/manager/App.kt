53e907598b2df438a0c35302cac7d337af8bca14
Leonardo Behnck <leonardo.behnck@gmail.com>
2024-09-05 10:36:21 -0300

    criando o datasource

--- app/src/main/kotlin/br/com/dio/work/manager/App.kt
+++ app/src/main/kotlin/br/com/dio/work/manager/App.kt
@@ -1,6 +1,11 @@
 package br.com.dio.work.manager
 
 import android.app.Application
+import br.com.dio.work.manager.datasource.VideosDataSource
 
 class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        VideosDataSource.setFromFile(assets)
+    }
 }
--- app/src/main/kotlin/br/com/dio/work/manager/datasource/VideosDataSource.kt
+++ app/src/main/kotlin/br/com/dio/work/manager/datasource/VideosDataSource.kt
@@ -5,16 +5,18 @@ import br.com.dio.work.manager.data.model.Video
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
-class VideoDataSource {
+object VideosDataSource {
     private val list by lazy { arrayListOf<Video>() }
 
-    fun setupFromFile(assetManager: AssetManager) {
+    fun setFromFile(assetManager: AssetManager) {
         val videosFromFile = Gson().fromFile<List<Video>>(assetManager, filename = "videos.json")
         list.addAll(videosFromFile)
     }
 
     private inline fun <reified T> Gson.fromFile(assetManager: AssetManager, filename: String): T {
-        return fromJson(assetManager.open(filename).bufferedReader(), object : TypeToken<T>() {}.type)
+        return fromJson(
+            assetManager.open(filename).bufferedReader(),
+            object : TypeToken<T>() {}.type
+        )
     }
-
 }
